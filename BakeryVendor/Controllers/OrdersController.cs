6d29e93efcd1b0556f7d54198d25c6822027a2f6
Louis Knolle <knollelw@gmail.com>
2022-07-16 12:31:12 -0400

    finish 1st attempt of order controller and view

--- BakeryVendor/Controllers/OrdersController.cs
+++ BakeryVendor/Controllers/OrdersController.cs
@@ -10,25 +10,25 @@ namespace BakeryVendor.Controllers
     [HttpGet("/vendors/{vendorId}/orders/new")]
     public ActionResult New(int vendorId)
     {
-      Vendor vendor = Vendor.Find(categoryId);
-      return View(category);
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
     }
 
-    [HttpPost("/items/delete")]
+    [HttpPost("/orders/delete")]
     public ActionResult DeleteAll()
     {
       Item.ClearAll();
       return View();
     }
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
     {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
       Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      model.Add("order", order);
+      model.Add("vendor", vendor);
       return View(model);
     }
   }
