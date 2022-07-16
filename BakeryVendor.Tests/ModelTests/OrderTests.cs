2df6684261599c000d209f22a9eb98ef4f2b8de3
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-07-15 21:28:21 -0400

    write/pass test for get all of order list

--- BakeryVendor.Tests/ModelTests/OrderTests.cs
+++ BakeryVendor.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,13 @@ using BakeryVendor.Models;
 namespace BakeryVendor.Tests 
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
@@ -32,6 +37,25 @@ namespace BakeryVendor.Tests
       Order newOrder = new Order("Ricky's", "Bread Reorder", 75, orderDate);
       DateTime result = newOrder.Date;
       Assert.AreEqual(orderDate, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrder_OrderList()
+    {
+      DateTime orderDate = new DateTime(2022, 7, 15);
+      Order newOrder1 = new Order("Ricky's", "Bread Reorder", 75, orderDate);
+      Order newOrder2 = new Order("Rick's", "Bread order", 750, orderDate);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
--- BakeryVendor/Models/Order.cs
+++ BakeryVendor/Models/Order.cs
@@ -5,12 +5,11 @@ namespace BakeryVendor.Models
 {
   public class Order
   {
-    // public Order(string title, string description, int price, DateTime date)
-
     public string Vendor { get; set; }
     public string Description { get; set; }
     public int Price { get; set; }
     public DateTime Date { get; set; }
+    private static List<Order> _instances = new List<Order> { };
 
     public Order(string vendor, string description, int price, DateTime date)
     {
@@ -18,6 +17,17 @@ namespace BakeryVendor.Models
       Description  = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
